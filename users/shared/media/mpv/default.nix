cc334175e95c73e6f564cd539b25d1c6b93ec40c
riki <rickunderstars@gmail.com>
2025-07-23 23:38:35 +0200

    mpv config updated

--- users/shared/media/mpv/default.nix
+++ users/shared/media/mpv/default.nix
@@ -9,12 +9,14 @@
   programs.mpv = {
     enable = true;
     config = {
-      "background-color" = "#1e1e2e";
-      "osd-back-color" = "#11111b";
-      "osd-border-color" = "#11111b";
-      "osd-color" = "#cdd6f4";
-      "osd-shadow-color" = "#1e1e2e";
-      "ao" = "pulse";
+      background-color = "#1e1e2e";
+      osd-back-color = "#11111b";
+      osd-border-color = "#11111b";
+      osd-color = "#cdd6f4";
+      osd-shadow-color = "#1e1e2e";
+      ao = "pulse";
+      fullscreen = "yes";
+
     };
     scriptOpts = {
       stats = {
