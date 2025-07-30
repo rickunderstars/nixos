745d7abaed3375276e6bef270546f722987c4af8
riki <rickunderstars@gmail.com>
2025-07-30 19:46:57 +0200

    increased download-buffer-size

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -28,6 +28,7 @@
         "root"
         "@wheel"
       ];
+      download-buffer-size = 1048576000;
       auto-optimise-store = true;
       warn-dirty = false;
     };
@@ -85,12 +86,6 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
     };
   };
 
