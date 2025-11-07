4e62dbc76526c7697650dd46ff5cb392945ec151
riki <rickunderstars@gmail.com>
2025-11-07 13:37:13 +0100

    waybar customization

--- users/riki/shared/desk-env/waybar/default.nix
+++ users/riki/shared/desk-env/waybar/default.nix
@@ -40,6 +40,28 @@
 
         "hyprland/workspaces" = {
           format = "{icon}";
+          format-icons = {
+            "1" = "I";
+            "2" = "II";
+            "3" = "III";
+            "4" = "IV";
+            "5" = "V";
+            "6" = "VI";
+            "7" = "VII";
+            "8" = "VIII";
+            "9" = "IX";
+            "10" = "X";
+            "11" = "XI";
+            "12" = "XII";
+            "13" = "XIII";
+            "14" = "XIV";
+            "15" = "XV";
+            "16" = "XVI";
+            "17" = "XVII";
+            "18" = "XVIII";
+            "19" = "XIX";
+            "20" = "XX";
+          };
           on-scroll-up = "hyprctl dispatch workspace m+1";
           on-scroll-down = "hyprctl dispatch workspace m-1";
         };
