42e95b2a21f97872b37c9abb76f29bc9fdeea306
riki <rickunderstars@gmail.com>
2025-11-26 03:21:55 +0100

    fastfetch styling

--- users/riki/shared/term/fastfetch/default.nix
+++ users/riki/shared/term/fastfetch/default.nix
@@ -6,28 +6,12 @@
   ...
 }:
 
-let
-  logo-src = ../../../assets/fastfetch/frieren-cat.png;
-  separator1 = {
-    type = "custom";
-    key = "├──────────────────────────────── ─  ── ─ ─  ─";
-  };
-
-  separator2 = {
-    type = "custom";
-    key = "├────────────────────────── ─ ──── ── ─ ─  ─";
-  };
-  bottom = {
-    type = "custom";
-    key = "╰─────────────────────────── ── ─   ─ ─  ── ─ ── ╯";
-  };
-in
 {
   programs.fastfetch = {
     enable = true;
     settings = {
       logo = {
-        source = logo-src;
+        source = ../../../assets/fastfetch/frieren-cat.png;
         height = 19;
         padding = {
           left = 0;
@@ -42,91 +26,103 @@ in
       modules = [
         {
           type = "title";
-          format = "╭┤{user-name-colored}{host-name-colored}├─────────────────────── ─── ─ ──  ─ ─╮";
+          format = "{##f5e0dc}╭┤{#}{user-name-colored}{##f5e0dc}{#}{host-name-colored}{##f5e0dc}├─────────────────────── ─── ─ ──  ─ ─╮{#}";
         }
         {
           type = "custom";
-          format = "│";
+          format = "{##f5e0dc}│{#}";
         }
         {
           type = "os";
           key = "│ os  ";
-          keyColor = "italic_green";
+          keyColor = "italic_#f5e0dc";
         }
         {
           type = "host";
           key = "│ host  ";
           format = "{vendor} {family}";
-          keyColor = "italic_green";
+          keyColor = "italic_#f2cdcd";
         }
         {
           type = "kernel";
           key = "│ kernel  ";
-          keyColor = "italic_green";
+          keyColor = "italic_#f5c2e7";
         }
         {
           type = "bootmgr";
           key = "│ boot  ";
           format = "{firmware-name}";
-          keyColor = "italic_green";
+          keyColor = "italic_#cba6f7";
         }
         {
           type = "uptime";
           key = "│ uptime  ";
-          keyColor = "italic_green";
+          keyColor = "italic_#f38ba8";
         }
-        separator1
+        {
+          type = "custom";
+          key = "├──────────────────────────────── ─  ── ─ ─  ─";
+          keyColor = "#f38ba8";
+        }
         {
           type = "shell";
           key = "│ shell  ";
-          keyColor = "italic_cyan";
+          keyColor = "italic_#eba0ac";
         }
         {
           type = "terminal";
           key = "│ term  ";
-          keyColor = "italic_cyan";
+          keyColor = "italic_#fab387";
         }
         {
           type = "packages";
           key = "│ pkgs  ";
-          keyColor = "italic_cyan";
+          keyColor = "italic_#f9e2af";
         }
         {
           type = "wm";
           key = "│ wm  ";
-          keyColor = "italic_cyan";
+          keyColor = "italic_#a6e3a1";
         }
-        separator2
+        {
+          type = "custom";
+          key = "├────────────────────────── ─ ──── ── ─ ─  ─";
+          keyColor = "#a6e3a1";
+        }
         {
           type = "cpu";
           key = "│ cpu  ";
           format = "{name}";
-          keyColor = "italic_blue";
+          keyColor = "italic_#94e2d5";
         }
         {
           type = "gpu";
           key = "│ gpu  ";
           format = "{name}";
-          keyColor = "italic_blue";
+          keyColor = "italic_#89dceb";
         }
         {
           type = "memory";
           key = "│ memory  ";
-          keyColor = "italic_blue";
+          keyColor = "italic_#74c7ec";
         }
         {
           type = "disk";
           key = "│ ssd  ";
           format = "{size-used} / {size-total} ({size-percentage})";
-          keyColor = "italic_blue";
+          keyColor = "italic_#89b4fa";
         }
         {
           type = "display";
           key = "│ display  ";
           format = "{width}x{height} @ {refresh-rate}Hz ({type}) ";
-          keyColor = "italic_blue";
+          keyColor = "italic_#b4befe";
         }
-        bottom
+        {
+          type = "custom";
+          key = "╰─────────────────────────── ── ─   ─ ─  ── ─ ── ╯";
+          keyColor = "#b4befe";
+        }
       ];
     };
   };
