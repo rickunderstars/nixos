952f49d7bac1cf060c974b6910315d1e3aedbd7b
riki <rickunderstars@gmail.com>
2025-12-09 18:53:09 +0100

    modified nixos rebuild shell abbr

--- users/riki/case/term/fish.nix
+++ users/riki/case/term/fish.nix
@@ -9,7 +9,7 @@
 {
   programs.fish = {
     shellAbbrs = {
-      nrs = "rm /home/riki/.config/mimeapps.list.backup; sudo nixos-rebuild switch --flake ~/nixos-config#case --show-trace";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-config#case --show-trace";
     };
   };
 }
--- users/riki/tars/term/fish.nix
+++ users/riki/tars/term/fish.nix
@@ -13,7 +13,7 @@
       mirror = "wl-mirror eDP-1 & sleep 0.5 && hyprctl dispatch fullscreen 0 && hyprctl dispatch movewindow mon:+1 && hyprctl dispatch focusmonitor -1";
     };
     shellAbbrs = {
-      nrs = "rm /home/riki/.config/mimeapps.list.backup; sudo nixos-rebuild switch --flake ~/nixos-config#tars --show-trace";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-config#tars --show-trace";
 
       # charges battery to 100% once (resets automatically)
       fullcharge = "sudo tlp fullcharge BAT0";
