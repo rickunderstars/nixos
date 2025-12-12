6da541f77b813047b00aa3551fe56b82e0674163
riki <rickunderstars@gmail.com>
2025-12-12 18:20:34 +0100

    disabled hyprshell overview + launcher feature

--- users/riki/shared/desk-env/hyprshell.nix
+++ users/riki/shared/desk-env/hyprshell.nix
@@ -13,29 +13,10 @@
     settings = {
       windows = {
         items_per_row = 5;
-        overview = {
-          launcher = {
-            default_terminal = "ghostty";
-            plugins = {
-              applications = {
-                run_cache_weeks = 1;
-                show_execs = true;
-                show_actions_submenu = true;
-              };
-              calc = { };
-            };
-          };
-          hide_filtered = false;
-        };
         switch = {
           filter_by = [ "current_workspace" ];
         };
       };
-    };
-
-    systemd = {
-      enable = true;
-      args = "-vv";
     };
 
     style = builtins.readFile ../../assets/hyprshell/style.css;
