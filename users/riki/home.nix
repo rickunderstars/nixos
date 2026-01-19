03f52253dbd84602e7712453c30e2e2b2cf0383d
riki <rickunderstars@gmail.com>
2026-01-19 23:34:02 +0100

    refactored home packages and fixed hardcoded paths

--- users/riki/home.nix
+++ users/riki/home.nix
@@ -12,109 +12,7 @@
     username = "riki";
     homeDirectory = "/home/riki";
 
-    packages = with pkgs; [
-
-      ####### cli/tui #######
-      ookla-speedtest
-      zip
-      unzip
-      wl-clipboard
-      bc
-      bat-extras.batman
-      playerctl
-      portal
-      bluetuith
-      impala
-      serie
-      wiremix
-      pulseaudio
-      wl-mirror
-      cpufetch
-      tdf
-      youtube-tui
-      exiftool # for superfile metadata plugin
-      zoxide # for superfile zoxide plugin
-      jq
-      wev
-      dust
-      bit-logo
-      mcat
-
-      ####### desk env #######
-      hyprpicker
-      hyprsysteminfo
-      hyprshade
-      papirus-icon-theme
-      nautilus
-      nautilus-python
-      nautilus-open-any-terminal
-      turtle
-      glib
-      gsettings-desktop-schemas
-
-      ####### coding #######
-      stable.vscodium
-      gitflow
-      nixfmt-rfc-style
-      gcc
-      llvmPackages.clang-tools
-      gnumake
-      gdb
-      cmake
-      openjdk
-      python3
-      texliveFull
-      rustc
-      cargo
-      arduino-ide
-      arduino-language-server
-      nodejs
-
-      ####### shell eye candy #######
-      cbonsai
-      cmatrix
-      pipes
-      tty-clock
-      astroterm
-      countryfetch
-      pokemon-colorscripts
-
-      ####### apps #######
-      (bottles.override { removeWarningPopup = true; })
-      video-trimmer
-      gimp
-      grimblast
-      gsimplecal
-      blender
-      krita
-      uxplay
-      pavucontrol
-      catppuccinifier-gui
-      scrcpy
-      localsend
-      telegram-desktop
-      whatsapp-electron
-      teams-for-linux
-      godot_4
-      itch
-      heroic
-      retroarch
-      prismlauncher
-      protonup-qt
-      prusa-slicer
-      cura-appimage
-      freecad
-      stable.meshlab
-      f3d
-      wings
-      ente-auth
-      spotify
-      papers
-      gnome-text-editor
-      libresprite
-      discord
-      discover-overlay
-    ];
+    packages = [ ];
   };
 
   programs = {
@@ -160,11 +58,11 @@
       pinnedFolders = [
         {
           name = "Screenshots";
-          location = "/home/riki/Pictures/Screenshots";
+          location = "${config.home.homeDirectory}/Pictures/Screenshots";
         }
         {
           name = "Onedrive";
-          location = "/home/riki/OneDrive";
+          location = "${config.home.homeDirectory}/OneDrive";
         }
       ];
       metadataPackage = pkgs.exiftool;
@@ -303,5 +201,6 @@
     ./shared/desk-env/waybar.nix
     ./shared/desk-env/dunst.nix
     ./shared/desk-env/global-themes.nix
+    ./shared/packages.nix
   ];
 }
