39a07eded290579f4b3464466ecb4984afc97897
riki <rickunderstars@gmail.com>
2026-03-01 21:14:01 +0100

    added mimeapps entries for office suite

--- users/riki/shared/desk-env/mimeapps.nix
+++ users/riki/shared/desk-env/mimeapps.nix
@@ -60,6 +60,22 @@
       "model/obj" = [ "f3d.desktop" ];
       "model/ply" = [ "f3d.desktop" ];
       "application/x-blender" = [ "blender.desktop" ];
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [
+        "onlyoffice-desktopeditors.desktop"
+      ];
+      "application/msword" = [ "onlyoffice-desktopeditors.desktop" ];
+      "application/vnd.oasis.opendocument.text" = [ "onlyoffice-desktopeditors.desktop" ];
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [
+        "onlyoffice-desktopeditors.desktop"
+      ];
+      "application/vnd.ms-excel" = [ "onlyoffice-desktopeditors.desktop" ];
+      "application/vnd.oasis.opendocument.spreadsheet" = [ "onlyoffice-desktopeditors.desktop" ];
+      "text/csv" = [ "onlyoffice-desktopeditors.desktop" ];
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [
+        "onlyoffice-desktopeditors.desktop"
+      ];
+      "application/vnd.ms-powerpoint" = [ "onlyoffice-desktopeditors.desktop" ];
+      "application/vnd.oasis.opendocument.presentation" = [ "onlyoffice-desktopeditors.desktop" ];
     };
 
     associations.added = {
