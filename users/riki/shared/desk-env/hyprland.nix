73939b15c0ef91af1a6961b36f65bd3103dfb933
riki <rickunderstars@gmail.com>
2026-05-01 19:19:56 +0200

    modified hyprland snap config

--- users/riki/shared/desk-env/hyprland.nix
+++ users/riki/shared/desk-env/hyprland.nix
@@ -349,8 +349,8 @@
 
         snap = {
           enabled = true;
-          window_gap = 20;
-          monitor_gap = 30;
+          window_gap = 10;
+          monitor_gap = 15;
           respect_gaps = true;
         };
       };
