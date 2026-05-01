b9e9b9b7bb7216e05752db77f79e0e3acbfc182e
riki <rickunderstars@gmail.com>
2026-05-01 17:39:40 +0200

    added youtube and instagram desktop entries

--- users/riki/home.nix
+++ users/riki/home.nix
@@ -164,10 +164,30 @@
     };
     desktopEntries = {
       onlyoffice-desktopeditors = {
-        name = "ONLYOFFICE";
+        name = "OnlyOffice";
         exec = "env QT_SCALE_FACTOR=1 onlyoffice-desktopeditors %U";
         terminal = false;
         categories = [ "Office" ];
+      };
+      youtube = {
+        name = "YouTube";
+        exec = "zen-twilight --new-window https://www.youtube.com";
+        terminal = false;
+        categories = [
+          "Network"
+          "Video"
+        ];
+      };
+      instagram = {
+        name = "Instagram";
+        exec = "zen-twilight --new-window https://www.instagram.com";
+        terminal = false;
+        categories = [
+          "Network"
+          "InstantMessaging"
+          "Chat"
+          "Photography"
+        ];
       };
     };
   };
