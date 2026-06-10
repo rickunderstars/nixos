136141e716544e9141f1229db9d6b404c996ac65
riki <rickunderstars@gmail.com>
2026-06-10 21:25:14 +0200

    modified hypridle tars config

--- users/riki/tars/modules/hypridle.nix
+++ users/riki/tars/modules/hypridle.nix
@@ -11,26 +11,26 @@
 
       listener = [
         {
-          timeout = 10; # 10sec - turn off keyboard backlight
+          timeout = 16; # turn off keyboard backlight
           on-timeout = "brightnessctl -sd platform::kbd_backlight set 0%";
           on-resume = "brightnessctl -rd platform::kbd_backlight";
         }
         {
-          timeout = 20; # 20sec - lower screen brightness
+          timeout = 32; # lower screen brightness (63840 is 16% of max tars value)
           on-timeout = "brightnessctl -s; [ $(brightnessctl get) -gt 63840 ] && brightnessctl set 16%";
           on-resume = "brightnessctl -r";
         }
         {
-          timeout = 60; # 1min - lock screen
+          timeout = 64; # lock screen
           on-timeout = "pidof hyprlock || hyprlock";
         }
         {
-          timeout = 120; # 2min - turn off screen
+          timeout = 128; # turn off screen
           on-timeout = "niri msg action power-off-monitors";
           on-resume = "niri msg action power-on-monitors";
         }
         {
-          timeout = 600; # 10min - suspend
+          timeout = 512; # suspend
           on-timeout = "systemctl suspend";
         }
       ];
