707032e83b2ace61f3dbe742b19e8760445e323e
riki <rickunderstars@gmail.com>
2026-06-10 22:15:27 +0200

    added public case key to ssh keys

--- hosts/shared/users.nix
+++ hosts/shared/users.nix
@@ -6,6 +6,7 @@
     description = "riki";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdoRyVI03LsqevvuNXq+fIRrqdcqACkTHB0gDwC4btj rickunderstars@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvLHOn57SHTqJl8/GC3SGBJP3/Wbartww29PpOyHQ3n rickunderstars@gmail.com"
     ];
     extraGroups = [
       "networkmanager"
