588bd7d059961e9434c4df2b429a657ae4a8daad
riki <rickunderstars@gmail.com>
2026-06-11 02:06:44 +0200

    removed hypridle config repetition, added niri bind

--- users/riki/tars/modules/hypridle.nix
+++ users/riki/tars/modules/hypridle.nix
@@ -3,11 +3,6 @@
 {
   services.hypridle = {
     settings = {
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "pidof hyprlock || hyprlock";
-        after_sleep_cmd = "niri msg action power-on-monitors";
-      };
 
       listener = [
         {
