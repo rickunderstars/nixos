76a26ff3aec4a6061f6c6994d8a726ee710f8522
riki <rickunderstars@gmail.com>
2026-07-22 20:53:20 +0200

    modified notification block script

--- users/riki/assets/scripts/fish/toggle-msg-notif.fish
+++ users/riki/assets/scripts/fish/toggle-msg-notif.fish
@@ -5,3 +5,9 @@ set rules block_whatsapp block_telegram block_signal block_discord
 for rule in $rules
     dunstctl rule $rule toggle
 end
+
+if test (dunstctl rules --json | jq -r '.data[0][] | select(.name.data == "block_whatsapp") | .enabled.data') = "true"
+    echo "notifications disabled"
+else
+    echo "notifications enabled"
+end
